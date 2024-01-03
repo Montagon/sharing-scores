0bd29fcd8b23ea685e4a30a217930f61df0ff1fc
José Carlos Montañez <josecarlos.aragon@47deg.com>
2024-01-03 08:39:07 +0100

    share anything

--- src/main/kotlin/org/jcma/sharingscores/services/ScoreManagement.kt
+++ src/main/kotlin/org/jcma/sharingscores/services/ScoreManagement.kt
@@ -1,5 +1,6 @@
 package org.jcma.sharingscores.services
 
+import org.jcma.sharingscores.models.GCloudFiles
 import org.jcma.sharingscores.models.Instrument
 import org.jcma.sharingscores.models.Score
 import org.jcma.sharingscores.models.ScoreBlock
@@ -13,24 +14,43 @@ class ScoreManagement(
     val parentId = gCloudSharing.extractFolderIdFromDriveLink(scoreUrl)!!
     val name = gCloudSharing.getName(scoreUrl)
     val score =
-      Score(
-        name = name,
-        blocks =
-          instruments.map { instrument ->
-            val regex = Regex(instrument.name)
-
-            val file = gFiles.find { regex.containsMatchIn(it.name) }
-            val updatedUrl =
-              if (file != null) {
-                if (gCloudSharing.shareFile(file.webViewLink)) {
-                  file.webViewLink
-                } else null
-              } else null
-            ScoreBlock(instrument.restrictLevel, instrument.publishName, updatedUrl)
-          },
-        videoUrl = videoUrl
-      )
+      Score(name = name, blocks = shareAndFilter(instruments, gFiles), videoUrl = videoUrl)
     gCloudSharing.createFile(score, parentId)
     return score
+  }
+
+  private fun shareAndFilter(
+    instruments: List<Instrument>,
+    gFiles: List<GCloudFiles>
+  ): List<ScoreBlock> {
+    return gFiles
+      .flatMap { gFile ->
+        gCloudSharing.shareFile(gFile.webViewLink)
+        instruments.map { instrument ->
+          val regex = Regex(instrument.name)
+          if (regex.containsMatchIn(gFile.name)) {
+            ScoreBlock(instrument.restrictLevel, instrument.publishName, gFile.webViewLink)
+          } else null
+        }
+      }
+      .filterNotNull()
+  }
+
+  private fun oldShareAndFilter(
+    instruments: List<Instrument>,
+    gFiles: List<GCloudFiles>
+  ): List<ScoreBlock> {
+    return instruments.map { instrument ->
+      val regex = Regex(instrument.name)
+
+      val file = gFiles.find { regex.containsMatchIn(it.name) }
+      val updatedUrl =
+        if (file != null) {
+          if (gCloudSharing.shareFile(file.webViewLink)) {
+            file.webViewLink
+          } else null
+        } else null
+      ScoreBlock(instrument.restrictLevel, instrument.publishName, updatedUrl)
+    }
   }
 }
